15396286838727d3f02af3c5fa63ac516a583880
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-09-29 09:51:02 +0200

    Chore: add metadata fields (algo type and algo tasks)

--- infer_detectron2_pointrend_process.py
+++ infer_detectron2_pointrend_process.py
@@ -169,6 +169,8 @@ class PointRendFactory(dataprocess.CTaskFactory):
         self.info.icon_path = "icons/detectron2.png"
         self.info.version = "1.5.0"
         self.info.keywords = "mask,rcnn,PointRend,facebook,detectron2,segmentation"
+        self.info.algo_type = core.AlgoType.INFER
+        self.info.algo_tasks = "INSTANCE_SEGMENTATION"
 
     def create(self, param=None):
         # Create process object
